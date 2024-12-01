5505e9e01ee77ae5c15650835f46801876473b0a
Roscoe Rubin-Rottenberg <roscoe@knotbin.xyz>
2024-12-01 11:33:24 -0500

    Initial Commit

---
+++ SkylightTests/SkylightTests.swift
@@ -0,0 +1,16 @@
+//
+//  SkylightTests.swift
+//  SkylightTests
+//
+//  Created by Roscoe Rubin-Rottenberg on 12/1/24.
+//
+
+import Testing
+
+struct SkylightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
